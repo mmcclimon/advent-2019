7e1d724c1b247bdd11aad6d0501e8b8b52decbc9
Michael McClimon <michael@mcclimon.org>
2023-01-07 16:03:51 -0500

    day 22: commit some nonsense for part 2

--- bin/day22.js
+++ bin/day22.js
@@ -48,6 +48,68 @@ const part1 = lines => {
   return idx;
 };
 
-
-const lines = utils.fileLines('input/day22.txt');
+let lines = utils.fileLines('input/day22.txt');
 console.log(part1(lines));
+
+// Okay, look: part 2 is basically impossible for me, so I'm going to commit
+// this, which reduces the input to a single inc/cut and be done with it.
+
+const extractNum = s => Number(s.split(' ').at(-1));
+
+const reduce = (lines, size = 10007) => {
+  const out = [];
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    const next = lines[i+1];
+
+    if (! next) {
+      if (line === 'deal into new stack') {
+        out.push('deal with increment -1');
+        out.push('cut 1');
+      } else {
+        out.push(line);
+      }
+      break;
+    }
+
+    const thisNum = extractNum(line);
+    const nextNum = extractNum(next);
+
+    if (line === 'deal into new stack') {
+      out.push('deal with increment -1');
+      out.push('cut 1');
+    }
+
+    if (line.startsWith('cut')) { 
+      if (next.startsWith('deal with increment')) {
+        out.push(next);
+        out.push(`cut ${mod(thisNum * nextNum, size)}`);
+        i++;
+      } else if (next.startsWith('cut')) {
+        out.push(`cut ${mod(thisNum + nextNum, size)}`);
+        i++;
+      } else {
+        out.push(line);
+      }
+    } 
+
+    if (line.startsWith('deal with increment')) {
+      if (next.startsWith('deal with increment')) {
+        out.push(`deal with increment ${mod(thisNum * nextNum, size)}`)
+        i++;
+      } else {
+        out.push(line);
+      }
+    }
+  }
+
+  return out;
+};
+
+let reduced = lines;
+while (reduced.length > 2) {
+  reduced = reduce(reduced);
+}
+
+console.log(reduced);
+console.log(part1(reduced));
