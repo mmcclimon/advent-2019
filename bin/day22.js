b3e7a941ca2dac3880b0d0d9fc512a6043bb9559
Michael McClimon <michael@mcclimon.org>
2023-01-07 15:58:32 -0500

    day 22: rework part 1 a bit

--- bin/day22.js
+++ bin/day22.js
@@ -2,6 +2,8 @@ const utils = require('../lib/advent-utils.js');
 
 const dealIntoNewStack = deck => deck.reverse();
 
+const mod = (n, m) => ((n % m) + m) % m;
+
 const cutN = (deck, n) => {
   Array.from(utils.range(Math.abs(n))).forEach(() => {
     if (n >= 0) deck.push(deck.shift());
@@ -14,38 +16,38 @@ const dealWithInc = (deck, n) => {
   let idx = 0;
   while (tmp.length > 0) {
     deck[idx] = tmp.shift();
-    idx = (idx + n) % deck.length;
+    idx = (((idx + n) % deck.length) + deck.length) % deck.length;
   }
 };
 
-const doShuffle = (size, lines) => {
-  const deck = Array.from(utils.range(size));
-  let m;
+const unInc = (deck, n) => {
+  const tmp = deck.slice();
+  let idx = 0;
+  for (let i = 0; i < deck.length; i++) {
+    deck[i] = tmp[idx];
+    idx = (((idx + n) % deck.length) + deck.length) % deck.length;
+  }
+}
+
+const part1 = lines => {
+  const size = 10007;
+  let idx = 2019;
 
   for (const line of lines) {
     if (line === 'deal into new stack') {
-      dealIntoNewStack(deck);
+      idx = size - idx - 1;
     } else if (m = line.match(/^cut (-?\d+)/)) {
-      cutN(deck, Number(m[1]));
-    } else if (m = line.match(/^deal with increment (\d+)/)) {
-      dealWithInc(deck, Number(m[1]));
+      idx = (((idx - Number(m[1])) % size) + size) % size;
+    } else if (m = line.match(/^deal with increment (-?\d+)/)) {
+      idx = (idx * Number(m[1])) % size;
     } else {
       throw new Error(`wat: ${line}`);
     }
   }
 
-  return deck;
+  return idx;
 };
 
-const part1 = lines => {
-  const d = doShuffle(10007, lines);
-  return d.findIndex(el => el === 2019);
-};
-
-const part2 = lines => {
-  const d = doShuffle(119315717514047, lines);
-};
 
 const lines = utils.fileLines('input/day22.txt');
 console.log(part1(lines));
-console.log(part2(lines));
